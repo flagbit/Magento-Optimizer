69a5377ef678899cdfae43f3dffd2a55f780da80
essig <essig@flagbit.de>
2011-03-24 15:46:04 +0100

    [+FEATURE] added sum of all css filetimes to the css merging md5 hash

---
+++ src/app/code/community/Flagbit/Optimizer/Model/Design/Package.php
@@ -0,0 +1,43 @@
+<?php
+class Flagbit_Optimizer_Model_Design_Package extends Mage_Core_Model_Design_Package
+{
+    /**
+     * Merge specified css files and return URL to the merged file on success
+     *
+     * @param $files
+     * @return string
+     */
+    public function getMergedCssUrl($files)
+    {
+        // secure or unsecure
+        $isSecure = Mage::app()->getRequest()->isSecure();
+        $mergerDir = $isSecure ? 'css_secure' : 'css';
+        $targetDir = $this->_initMergerDir($mergerDir);
+        if (!$targetDir) {
+            return '';
+        }
+
+        // base hostname & port
+        $baseMediaUrl = Mage::getBaseUrl('media', $isSecure);
+        $hostname = parse_url($baseMediaUrl, PHP_URL_HOST);
+        $port = parse_url($baseMediaUrl, PHP_URL_PORT);
+        if (false === $port) {
+            $port = $isSecure ? 443 : 80;
+        }
+
+        $filetimeSum = 0;
+        foreach($files as $file) {
+        	if(!file_exists($file)) {
+        		continue;
+        	}
+        	$filetimeSum += filemtime($file);
+        }
+        
+        // merge into target file
+        $targetFilename = md5(implode(',', $files) . "|{$hostname}|{$port}" . md5($filetimeSum)) . '.css';
+        if ($this->_mergeFiles($files, $targetDir . DS . $targetFilename, false, array($this, 'beforeMergeCss'), 'css')) {
+            return $baseMediaUrl . $mergerDir . '/' . $targetFilename;
+        }
+        return '';
+    }
+}
